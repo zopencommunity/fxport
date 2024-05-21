00465b8eb61f7d59537998398f4b9a0e95ad6953
Joon Lee <joon.lee727@gmail.com>
2024-05-21 16:22:42 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Automatic version updates](https://github.com/ZOSOpenTools/fxport/actions/workflows/bump.yml/badge.svg)](https://github.com/ZOSOpenTools/fxport/actions/workflows/bump.yml)
+
+# fx
+
+Terminal JSON viewer & processor[1~[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[3~[C
---
+++ cicd-stable.groovy
@@ -0,0 +1,14 @@
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/fxport.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/fxport.git'), string(name: 'PORT_DESCRIPTION', value: 'Terminal JSON viewer & processor[1~[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[3~[C' ), string(name: 'BUILD_LINE', value: 'STABLE') ]
+  }
+}
